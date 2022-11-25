b15ab9833bf77c6313436592009df0d101384d7e
JuanCDiaz1 <juankid2003gmail.com>
2022-11-25 05:38:34 -0600

    first commit

---
+++ Data/BankContext.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using BankAPI.Data.BankModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankAPI.Data;
+
+public partial class BankContext : DbContext
+{
+    public BankContext()
+    {
+    }
+
+    public BankContext(DbContextOptions<BankContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Account> Accounts { get; set; }=null!;
+
+    public virtual DbSet<AccountType> AccountTypes { get; set; }=null!;
+
+    public virtual DbSet<Administrator> Administrators { get; set; }=null!;
+
+    public virtual DbSet<BankTransaction> BankTransactions { get; set; }=null!;
+
+    public virtual DbSet<Client> Clients { get; set; }=null!;
+
+    public virtual DbSet<TransactionType> TransactionTypes { get; set; }=null!;
+ 
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Account__3214EC27A14137BF");
+
+            entity.ToTable("Account");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Balance).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.ClientId).HasColumnName("ClientID");
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.AccountTypeNavigation).WithMany(p => p.Accounts)
+                .HasForeignKey(d => d.AccountType)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Account__Account__403A8C7D");
+
+            entity.HasOne(d => d.Client).WithMany(p => p.Accounts)
+                .HasForeignKey(d => d.ClientId)
+                .HasConstraintName("FK__Account__ClientI__412EB0B6");
+        });
+
+        modelBuilder.Entity<AccountType>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__AccountT__3214EC279A5A06C6");
+
+            entity.ToTable("AccountType");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<Administrator>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Administ__3214EC27E8E7CB4A");
+
+            entity.ToTable("Administrator");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.AdminType)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Pwd)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<BankTransaction>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__BankTran__3214EC27B87A4845");
+
+            entity.ToTable("BankTransaction");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.AccountId).HasColumnName("AccountID");
+            entity.Property(e => e.Amount).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.Account).WithMany(p => p.BankTransactions)
+                .HasForeignKey(d => d.AccountId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__BankTrans__Accou__44FF419A");
+
+            entity.HasOne(d => d.TransactionTypeNavigation).WithMany(p => p.BankTransactions)
+                .HasForeignKey(d => d.TransactionType)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__BankTrans__Trans__45F365D3");
+        });
+
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Client__3214EC2785EAA53E");
+
+            entity.ToTable("Client", tb =>
+                {
+                    tb.HasTrigger("ClientAfterInsert");
+                    tb.HasTrigger("ClientInsteadOfDelete");
+                });
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<TransactionType>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Transact__3214EC2781440A7D");
+
+            entity.ToTable("TransactionType");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.RegDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
